edf1cf5e76fe6e400476afba1a3d25e5a52007e0
khaalisharvest <khaalisharvest@gmail.com>
2026-08-13 15:51:54 +0500

    fix: correct robots.txt, fix sitemap to khaalisharvest.com

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,18 +1,9 @@
 import { MetadataRoute } from 'next';
-import { APP_URL } from '@/config/env';
 
 export default function robots(): MetadataRoute.Robots {
-  const baseUrl = 'https://khaalisharvest.com';
-
   return {
-    rules: [
-      {
-        userAgent: '*',
-        allow: '/',
-        disallow: ['/admin/', '/api/', '/super-admin/', '/auth/'],
-      },
-    ],
-    sitemap: `${baseUrl}/sitemap.xml`,
+    rules: [{ userAgent: '*', allow: '/' }],
+    sitemap: 'https://khaalisharvest.com/sitemap.xml',
   };
 }
 
